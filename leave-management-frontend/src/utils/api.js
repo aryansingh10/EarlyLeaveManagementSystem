4e54472e3b26f0e5f62199fa56796302c9b8604a
Aryan Singh Thakur <aryansingh102003@gmail.com>
2024-10-28 20:49:17 +0530

    Some changes ADded

--- leave-management-frontend/src/utils/api.js
+++ leave-management-frontend/src/utils/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+
 const api = axios.create({
-  baseURL: 'https://early-leave-automation-system.vercel.app/api',
+  baseURL: 'https://early-leave-automation-system.vercel.app/api', 
   headers: {
     'Content-Type': 'application/json',
-  },
-  withCredentials: true, // Important for sending cookies
+  }
 });
+
+
 
 export default api;
