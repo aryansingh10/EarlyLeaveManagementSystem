7aead52bd0791ad4ca1f32266c703c5cd965994c
Aryan Singh Thakur <aryansingh102003@gmail.com>
2024-10-28 18:50:19 +0530

    Some changes ADded

--- leave-management-frontend/src/utils/api.js
+++ leave-management-frontend/src/utils/api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-
+const token = localStorage.getItem('authToken');
 const api = axios.create({
-  baseURL: 'https://early-leave-automation-system.vercel.app/api', 
+  baseURL: 'https://early-leave-automation-system.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    ...(token && { 'Authorization': `Bearer ${token}` })
   }
 });
-
-
 
 export default api;
