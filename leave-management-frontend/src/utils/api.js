92accdc2a64def2820f55afd874ae2f58258ca99
Aryan Singh Thakur <aryansingh102003@gmail.com>
2024-11-02 11:25:42 +0530

    Added Some changes

--- leave-management-frontend/src/utils/api.js
+++ leave-management-frontend/src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://early-leave-management-system.vercel.app/api',
   withCredentials: true,
+
   headers: {
     'Content-Type': 'application/json',
   },
