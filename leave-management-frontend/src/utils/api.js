8b88658dca7ccfb2110f9d51e550185aecf02f40
Aryan Singh Thakur <aryansingh102003@gmail.com>
2024-11-04 20:31:35 +0530

    Deployed

--- leave-management-frontend/src/utils/api.js
+++ leave-management-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://earlyleavemanagementsystem.onrender.com/api',
+  baseURL: 'https://early-leave-management-system.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
