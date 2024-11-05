061c0de6194569b03a712727009fdefcceeff7d9
Aryan Singh Thakur <aryansingh102003@gmail.com>
2024-11-05 11:46:21 +0530

    Footer Udpated

--- leave-management-frontend/src/components/Footer.js
+++ leave-management-frontend/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-6 fixed" >
+    <footer className="bg-gray-800 text-white py-6 " >
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
         <div className="mb-4 md:mb-0">
           <h3 className="text-xl md:text-lg font-bold">AutomatedLeaveSystem</h3>
