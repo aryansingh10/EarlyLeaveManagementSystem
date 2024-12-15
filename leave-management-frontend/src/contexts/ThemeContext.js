1b56325e861e72affbb09dd32c23b130fca0d5dd
Arpit_Jaiswal <jaiswalarpit1414@gmail.com>
2024-12-15 15:34:20 +0530

    modify navbar ui

---
+++ leave-management-frontend/src/contexts/ThemeContext.js
@@ -0,0 +1,29 @@
+// src/contexts/ThemeContext.js
+
+import React, { createContext, useState, useContext, useEffect } from 'react';
+
+const ThemeContext = createContext();
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
+  const toggleTheme = () => setDarkMode(!darkMode);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
